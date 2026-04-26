659319211a4f69a21f573c8e70efd7bee8cfc1ea
aashka <136294443+aashka05@users.noreply.github.com>
2026-04-26 13:19:14 +0530

    03_5

---
+++ 03_5_StyleTheme/java/com/example/a03_5_style_theme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a03_5_style_theme;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
